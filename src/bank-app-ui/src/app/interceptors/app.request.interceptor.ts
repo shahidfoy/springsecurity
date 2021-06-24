1b16c829a5e446411f4aeeea4e8763415420abfd
shahidfoy <shahidfoy@gmail.com>
2021-06-24 12:09:47 -0600

    implmented jwt token generation and validation

--- src/bank-app-ui/src/app/interceptors/app.request.interceptor.ts
+++ src/bank-app-ui/src/app/interceptors/app.request.interceptor.ts
@@ -16,10 +16,14 @@ export class XhrInterceptor implements HttpInterceptor {
     if(this.user && this.user.password && this.user.email){
       httpHeaders = httpHeaders.append('Authorization', 'Basic ' + btoa(this.user.email + ':' + this.user.password));
     }
-    let xsrf = sessionStorage.getItem('XSRF-TOKEN');
-    if (xsrf) {
-      httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);
+    let authorization = sessionStorage.getItem('Authorization');
+    if (authorization) {
+      httpHeaders = httpHeaders.append('Authorization', authorization);
     }
+    // let xsrf = sessionStorage.getItem('XSRF-TOKEN');
+    // if (xsrf) {
+    //   httpHeaders = httpHeaders.append('X-XSRF-TOKEN', xsrf);
+    // }
     httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
     const xhr = req.clone({
       headers: httpHeaders
--- src/main/java/com/shahidfoy/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/shahidfoy/springsecurity/config/ProjectSecurityConfig.java
@@ -1,13 +1,12 @@
 package com.shahidfoy.springsecurity.config;
 
-import com.shahidfoy.springsecurity.filter.AuthoritiesLoggingAfterFilter;
-import com.shahidfoy.springsecurity.filter.AuthoritiesLoggingAtFilter;
-import com.shahidfoy.springsecurity.filter.RequestValidationBeforeFilter;
+import com.shahidfoy.springsecurity.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -35,7 +35,9 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		 http.cors().configurationSource(new CorsConfigurationSource() {
+		 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				 .and()
+		    .cors().configurationSource(new CorsConfigurationSource() {
 			 @Override
 			 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 				 CorsConfiguration config = new CorsConfiguration();
@@ -43,16 +45,21 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 				 config.setAllowedMethods(Collections.singletonList("*"));
 				 config.setAllowCredentials(true);
 				 config.setAllowedHeaders(Collections.singletonList("*"));
+				 config.setExposedHeaders(Arrays.asList("Authorization"));
 				 config.setMaxAge(3600L);
 				 return config;
 			 }
 		 	})
-		    .and()
-		    .csrf().ignoringAntMatchers("/contact")
-		    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		    .and()
+		    	.and()
+		    .csrf()
+		    .disable()
+//			.ignoringAntMatchers("/contact")
+//		    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//		    .and()
 		    .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
 		    .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+		    .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+		    .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
 		    .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
 		    .authorizeRequests()
 			.antMatchers("/myAccount").hasRole("USER")
